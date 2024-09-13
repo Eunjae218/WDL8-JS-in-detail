5f375396bdaea0ce2907f84c6b36422a65e16b9e
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-13 18:19:48 +0700

    added a1

---
+++ script.js
@@ -0,0 +1,7 @@
+let numberres = document.getElementById("numres")
+let stringres = document.getElementById("strres")
+let a = 70
+let b = 90
+
+numberres.innerHTML = a + b
+stringres.innerHTML = String(a) + String(b)
