862f132a89b6746aa4fdfaddcea97d691f163e65
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-13 18:26:01 +0700

    a2 learned try and catch

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+//a1
 let numberres = document.getElementById("numres")
 let stringres = document.getElementById("strres")
 let a = 70
 let b = 90
 
+//a2
+let errormessage = document.getElementById("errormsg")
+
+//a1
 numberres.innerHTML = a + b
 stringres.innerHTML = String(a) + String(b)
+
+//a2
+try{
+    aletr("mistake muhhahahaha")
+}
+
+catch(err){
+    errormessage.innerHTML = err
+}
