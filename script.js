6cbf9e10530a3bb6942bbdeacc89ade96b8a21c0
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-13 18:31:11 +0700

    made a3 arrow function

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let b = 90
 //a2
 let errormessage = document.getElementById("errormsg")
 
+//a3
+let resutl = document.getElementById("result")
+
 //a1
 numberres.innerHTML = a + b
 stringres.innerHTML = String(a) + String(b)
@@ -19,3 +22,8 @@ try{
 catch(err){
     errormessage.innerHTML = err
 }
+
+//a3
+add = (a,b) => a+b;
+
+resutl.innerHTML = add(4,10)
